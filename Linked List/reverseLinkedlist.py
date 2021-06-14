cc2717f5743595acb4cf0fbed14246084233cbff
niharpawade@outlook.com <niharpawade@outlook.com>
2021-06-15 00:56:23 +0530

    reverse linkedlist iterative

---
+++ Linked List/reverseLinkedlist.py
@@ -0,0 +1,52 @@
+class Node:
+ 
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+ 
+class LinkedList:
+ 
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+ 
+    # Function to reverse the linked list
+    def reverse(self):
+        prev  = None
+        curr = self.head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr =temp
+            print(temp.data)
+        self.head = curr
+ 
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+    # Utility function to print the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+ 
+ 
+# Driver code
+llist = LinkedList()
+llist.push(20)
+llist.push(4)
+llist.push(15)
+llist.push(85)
+ 
+print ("Given Linked List")
+llist.printList()
+llist.reverse()
+print ("\nReversed Linked List")
+llist.printList()
