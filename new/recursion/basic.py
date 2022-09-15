26cde4315877dd465a9b6dbacda7d2381d7f47ff
Nihar Pawade <niharpawade@outlook.com>
2022-09-15 17:05:13 +0530

    basics !!!

--- new/recursion/basic.py
+++ new/recursion/basic.py
@@ -11,7 +11,7 @@ def print_n(i: int,n: int) -> None:
 
 print_n(1, 4)
 
-
+print('\n')
 #backtracking
 def print_n_bt(i: int, n: int) -> None:
     
@@ -24,3 +24,28 @@ def print_n_bt(i: int, n: int) -> None:
 
     
 print_n_bt(4,4)
+print('\n')
+
+#print n to 1
+
+def print_rev(i: int, n: int) -> None:
+
+    if i < 1:
+        return
+
+    print(i)
+
+    print_rev(i-1, n)
+
+print_rev(4,4)        
+print('\n')
+
+#using back track
+def print_rev_bt(i: int, n: int) -> None:
+    
+    if i > n:
+        return
+
+    print_rev_bt(i+1, n)
+    print(i)    
+print_rev_bt(1,4)
